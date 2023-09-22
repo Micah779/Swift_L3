b72d581d745e770f940502092b8b24fc8f546b7b
Micah779 <contactmaldrich@gmail.com>
2023-09-22 17:51:18 -0400

    Created the simple DaLat information tab for L3

--- L3 Demo/ContentView.swift
+++ L3 Demo/ContentView.swift
@@ -9,13 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading) {
+                Image("daLat")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15.0)
+                
+                HStack {
+                    Text("Da Lat")
+                        .foregroundColor(.black)
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.fill")
+                        }
+                        Text("(Reviews 361)")
+                    }
+                    .foregroundColor(.yellow)
+                    .font(.caption)
+                }
+                
+                Text("The Home of Mikey | Top Rated")
+                    .foregroundColor(.black)
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                }
+                .foregroundColor(.black)
+                .font(.caption)
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(15)
+                .shadow(radius: 15))
+            .padding()
         }
-        .padding()
     }
 }
 
