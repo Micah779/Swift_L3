cb4e2d81e1a60f98bfb5e21a70acdcf26a405f31
Micah779 <contactmaldrich@gmail.com>
2023-09-22 17:11:30 -0400

    Initial Commit

---
+++ L3 Demo/L3_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L3_DemoApp.swift
+//  L3 Demo
+//
+//  Created by Micah Aldrich on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct L3_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
